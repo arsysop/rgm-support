c64e02f773898c2ac7ca9e645a4d6d76ed9cbd53
eparovyshnaya <elena.parovyshnaya@gmail.com>
2025-04-30 18:30:23 +0300

    [rgm-support/#3] general docs and Peeper Lib example

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# rgm-support
-[ArSysOp](https://arsysop.ru/) [LOFT RGM](https://arsysop.ru/rgm/) Products Support
+## [ArSysOp](https://arsysop.ru/) [LOFT RGM](https://arsysop.ru/rgm/) Products Support
 
+### Repository
 In case 
  - you have a feature to propose for implementation
  - or a bug to report
@@ -8,3 +8,62 @@ In case
 add a note to an [existing open issue](https://github.com/arsysop/rgm-support/issues) that fits your needs or [create new one](https://github.com/arsysop/rgm-support/issues/new) (for logged in users).
 
 See the list of [milestones](https://github.com/arsysop/rgm-support/milestones) to plan LOFT RGM products updates.
+
+### Products
+There are two standalone applications that help with C++ standard analysis
+ - LOFT RGM Peeper for smart structured view and navigation through the standard
+ - LOFT RGM Architect for mark-up and requirement gathering
+
+There is also a library that supplies programmatic API for specification model access to create custom analysis instruments.
+
+### How to use LOFT RGM Library
+
+#### Prerequisites
+Libraries can serve programmatic purposes of reading RGM Specification model in scope of an Eclipse application of any kind.
+Using the Library requires
+ - basic skills in Eclipse programming
+ - basic experience with PDE 
+ - knowledge of Eclipse application structure
+ - practical acquaintance with EMF Framework
+
+Also, the Library is built over Java 21, so it must be accessible.
+
+#### Install license
+License (`.licen` file) is to be placed in the folder `~\.passage\ru.arsysop.loft.rgm.view.product\1.0.0\`.
+
+If you do not have a license, contact [ArSysOp Support team](mailto:support@arsysop.ru).
+
+Without a license client code is going to fail with errors like this:
+
+```
+org.eclipse.passage.lic.api.LicensingException: No license coverage for feature 
+identifier=ru.arsysop.loft.rgm.spec.load.encrypted, 
+version=1.0.0, 
+name=RGM Specification Load for Encrypted Model, 
+provider=ArSysOp
+```
+
+#### Extend your target platform
+Append additional _location_ to the target platform of your custom codebase:
+
+```
+		<location includeAllPlatforms="false" includeConfigurePhase="true" includeMode="planner" includeSource="true" type="InstallableUnit">
+			<repository location="https://arsysop.ru/rgm/updates/milestone/latest/"/>
+			<unit id="ru.arsysop.loft.rgm.library.peeper.feature.feature.group" version="0.0.0"/>
+		</location>
+
+```
+
+#### Dependency
+Append additional _dependency_ to your custom codebase. For instance, it will look like this for _MANIFEST.MF_ of your plug-in:
+
+```
+Require-Bundle: ru.arsysop.loft.rgm.spec.model;bundle-version="1.0.0"
+```
+
+#### Sample
+See _ru.arsysop.loft.rgm.peeper.tests.PeeperTest_ in _example-src_ folder of this repository to meet actual code, 
+that loads, reads and asserts Encrypted Specification Model content.
+
+#### Collaboration
+ArSysOp Team can help you with creating your own C++ standard analysis tools. 
---
+++ example-src/ru/arsysop/loft/rgm/peeper/tests/PeeperTest.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) ArSysOp 2020-2025
+ * 
+ * ArSysOp and its affiliates make no warranty of any kind
+ * with regard to this material.
+ * 
+ * ArSysOp expressly disclaims all warranties as to the material, express,
+ * and implied, including but not limited to the implied warranties of
+ * merchantability, fitness for a particular purpose and non-infringement of third
+ * party rights.
+ * 
+ * In no event shall ArSysOp be liable to you or any other person for any damages,
+ * including, without limitation, any direct, indirect, incidental or consequential
+ * damages, expenses, lost profits, lost data or other damages arising out of the use,
+ * misuse or inability to use the material and any derived software, even if ArSysOp,
+ * its affiliate or an authorized dealer has been advised of the possibility of such damages.
+ *
+ */
+
+package ru.arsysop.loft.rgm.peeper.tests;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.util.List;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.junit.Test;
+
+import ru.arsysop.loft.rgm.spec.model.api.Document;
+import ru.arsysop.loft.rgm.spec.model.api.Section;
+import ru.arsysop.loft.rgm.spec.model.api.Toc;
+import ru.arsysop.loft.rgm.spec.model.api.TocChapter;
+
+public final class PeeperTest {
+
+	@Test
+	public void readSpecEn() {
+		ResourceSetImpl resourceSet = new ResourceSetImpl();
+		File specen = new File("cpp17_cpp17.specen"); //$NON-NLS-1$
+		URI uri = URI.createFileURI(specen.getAbsolutePath());
+		Resource resource = resourceSet.getResource(uri, true);
+		assertNotNull(resource);
+		List<EObject> contents = resource.getContents();
+		assertFalse(contents.isEmpty());
+		Document document = (Document) contents.get(0);
+		readToc(document);
+		readSection(document);
+	}
+
+	private void readToc(Document document) {
+		Toc toc = document.getToc();
+		assertNotNull(toc);
+		List<TocChapter> chapters = toc.getChapters();
+		assertFalse(chapters.isEmpty());
+		readAnyTocChapter(chapters);
+	}
+
+	private void readAnyTocChapter(List<TocChapter> chapters) {
+		TocChapter chapter = fullFeatherChapters(chapters);
+		// DOC: Identifier is not memory-encrypted
+		assertTrue(chapter.getId().equals(chapter.getIdDecrypted()));
+		// DOC: Most of other attributes are memory-encrypted;
+		// DOC: use get*Decrypted() methods to access business value
+		assertFalse(chapter.getName().equals(chapter.getNameDecrypted()));
+		assertFalse(chapter.getNumber().equals(chapter.getNumberDecrypted()));
+	}
+
+	private void readSection(Document document) {
+		List<Section> sections = document.getSections();
+		assertFalse(sections.isEmpty());
+		readAnySection(sections);
+	}
+
+	private void readAnySection(List<Section> sections) {
+		Section section = fullFeatherSection(sections);
+		// DOC: Identifier is not memory-encrypted
+		assertTrue(section.getId().equals(section.getIdDecrypted()));
+		// DOC: Most of other attributes are memory-encrypted;
+		// DOC: use get*Decrypted() methods to access business value
+		assertFalse(section.getName().equals(section.getNameDecrypted()));
+		assertFalse(section.getNumber().equals(section.getNumberDecrypted()));
+		assertFalse(section.getLocation().equals(section.getLocationDecrypted()));
+	}
+
+	private Section fullFeatherSection(List<Section> sections) {
+		return sections.stream()//
+				.filter(this::fullFeather)//
+				.findAny()//
+				.get();
+	}
+
+	private TocChapter fullFeatherChapters(List<TocChapter> chapters) {
+		return chapters.stream()//
+				.filter(this::fullFeather)//
+				.findAny()//
+				.get();
+	}
+
+	private boolean fullFeather(TocChapter chapter) {
+		return valuable(chapter.getId())//
+				&& valuable(chapter.getName())//
+				&& valuable(chapter.getNumber());
+	}
+
+	private boolean fullFeather(Section section) {
+		return valuable(section.getId())//
+				&& valuable(section.getName())//
+				&& valuable(section.getNumber())//
+				&& valuable(section.getLocation());
+	}
+
+	private boolean valuable(String value) {
+		return value != null && !value.isBlank();
+	}
+
+}
